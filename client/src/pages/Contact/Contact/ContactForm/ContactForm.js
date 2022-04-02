7e3f96d1660932cb15400a2516612d66664378a2
Meenakshi Pathania <meenakship@ocodewire.com>
2022-04-02 19:17:17 +0530

    commit with contact form data is saved in strapi

--- client/src/pages/Contact/Contact/ContactForm/ContactForm.js
+++ client/src/pages/Contact/Contact/ContactForm/ContactForm.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { FaMapMarkerAlt, FaPhoneAlt, FaEnvelopeOpen } from 'react-icons/fa';
-
 const ContactForm = () => {
     const [text, Settext] = useState([]);
     useEffect(() => {
@@ -16,6 +15,46 @@ const ContactForm = () => {
         return () => request.cancel();
     }, []);
 
+    const [firstname, setFirstName] = useState('');
+    const [lastname, setLastName] = useState('');
+    const [email, setEmail] = useState('');
+    const [phone, setPhone] = useState('');
+    const [interested, setInterested] = useState('');
+    const [hear, setHear] = useState('');
+    const [message, setMessage] = useState('');
+    const [streetaddress, setStreetaddress] = useState('');
+    const [streetaddress2, setStreetaddress2] = useState('');
+    const [city, setCity] = useState('');
+    const [state, setState] = useState('');
+    const [zip, setZip] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const contact = { firstname, lastname, email, phone, interested, hear, message, streetaddress, streetaddress2, city, state, zip };
+
+        fetch('http://165.227.11.15:1337/api/contect-datas', {
+            method: 'POST',
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ data: contact })
+        }).then(() => {
+            console.log("new contact added")
+        })
+    }
+    
+    //  axios.post(
+    //     'http://165.227.11.15:1337/api/contect-datas',
+    //     {
+    //         data: {
+    //             firstname: 'firstname',
+    //             lastname: 'lastname',
+    //             email: 'email',
+    //             phone: 'phone'
+    //         }
+    //     })
+    //     .then(response => {
+    //         console.log(response);
+    //     });
+    
     return (
         <>
             <section className="tp-contact-area pb-120 mt-80">
@@ -67,50 +106,82 @@ const ContactForm = () => {
                             </div>
                         </div>
                         <div className="col-lg-8">
-                            <div className="tp-contact-form">
-                                <div className="row custom-mar-20">
-                                    <div className="col-md-6 custom-pad-20">
-                                        <div className="tp-contact-form-field mb-20">
-                                            <input type="text" placeholder="Full name" />
+                            <form id="contactForm" method="POST" onSubmit={handleSubmit}>
+                                <div className="tp-contact-form">
+                                    <div className="row custom-mar-20">
+                                        <div className="col-md-6 custom-pad-20">
+                                            <div className="tp-contact-form-field mb-20">
+                                                <input type="text" placeholder="First Name" value={firstname} onChange={(e) => setFirstName(e.target.value)} />
+                                            </div>
                                         </div>
-                                    </div>
-                                    <div className="col-md-6 custom-pad-20">
-                                        <div className="tp-contact-form-field mb-20">
-                                            <input type="text" placeholder="Phone" />
+                                        <div className="col-md-6 custom-pad-20">
+                                            <div className="tp-contact-form-field mb-20">
+                                                <input type="text" placeholder="Last Name" value={lastname} onChange={(e) => setLastName(e.target.value)} />
+                                            </div>
                                         </div>
-                                    </div>
-                                    <div className="col-md-6 custom-pad-20">
-                                        <div className="tp-contact-form-field mb-20">
-                                            <input type="email" placeholder="Email Address" />
+                                        <div className="col-md-6 custom-pad-20">
+                                            <div className="tp-contact-form-field mb-20">
+                                                <input type="email" placeholder="Email Address" value={email} onChange={(e) => setEmail(e.target.value)} />
+                                            </div>
                                         </div>
-                                    </div>
-                                    <div className="col-md-6 custom-pad-20">
-                                        <div className="tp-contact-form-field select-field-arrow mb-20">
-                                            <select>
-                                                <option defaultValue="1">How did you hear about us ?</option>
-                                                <option defaultValue="2">Google</option>
-                                                <option defaultValue="3">Good Contractors List</option>
-                                                <option defaultValue="4">Angie's List</option>
-                                                <option defaultValue="5">Facebook</option>
-                                                <option defaultValue="6">Friend/Neighbour</option>
-                                                <option defaultValue="6">Others</option>
-                                            </select>
+                                        <div className="col-md-6 custom-pad-20">
+                                            <div className="tp-contact-form-field mb-20">
+                                                <input type="text" placeholder="Phone" value={phone} onChange={(e) => setPhone(e.target.value)} />
+                                            </div>
                                         </div>
-                                    </div>
-                                    <div className="col-md-12 custom-pad-20">
-                                        <div className="tp-contact-form-field mb-20">
-                                            <textarea placeholder="Your Message"></textarea>
+                                        <div className="col-md-6 custom-pad-20">
+                                            <div className="tp-contact-form-field select-field-arrow mb-20">
+                                                <select value={interested} onChange={(e) => setInterested(e.target.value)}>
+                                                    <option defaultValue="1">Interested In</option>
+                                                    <option defaultValue="2" value="Landscaping">Landscaping</option>
+                                                    <option defaultValue="3" value="Lawn Care">Lawn Care</option>
+                                                    <option defaultValue="4" value="Sod">Sod</option>
+                                                    <option defaultValue="5" value="Tree Service">Tree Service</option>
+                                                    <option defaultValue="6" value="Mosquito Service">Mosquito Service</option>
+                                                    <option defaultValue="7" value="Retaining Walls">Retaining Walls</option>
+                                                    <option defaultValue="8" value="Remodeling">Remodeling</option>
+                                                    <option defaultValue="9" value="Demolition">Demolition</option>
+                                                    <option defaultValue="10" value="Irrigation">Irrigation</option>
+                                                    <option defaultValue="11" value="Drainage">Drainage</option>
+                                                    <option defaultValue="12" value="Fencing">Fencing</option>
+                                                    <option defaultValue="13" value="Concrete">Concrete</option>
+                                                    <option defaultValue="14" value="Other">Other</option>
+                                                </select>
+                                            </div>
                                         </div>
-                                    </div>
+                                        <div className="col-md-6 custom-pad-20">
+                                            <div className="tp-contact-form-field select-field-arrow mb-20">
+                                                <select value={hear} onChange={(e) => setHear(e.target.value)}>
+                                                    <option defaultValue="1">How did you hear about us ?</option>
+                                                    <option defaultValue="2" value="Google">Google</option>
+                                                    <option defaultValue="3" value="Good Contractors List">Good Contractors List</option>
+                                                    <option defaultValue="4" value="Angie's List">Angie's List</option>
+                                                    <option defaultValue="5" value="Facebook">Facebook</option>
+                                                    <option defaultValue="6" value="Friend/Neighbour">Friend/Neighbour</option>
+                                                    <option defaultValue="7" value="Others">Others</option>
+                                                </select>
+                                            </div>
+                                        </div>
+                                        <div className="col-md-12 custom-pad-20">
+                                            <div className="tp-contact-form-field mb-20">
+                                                <textarea name="message" placeholder="Your Message" value={message} onChange={(e) => setMessage(e.target.value)}></textarea>
+                                            </div>
+                                        </div>
 
-                                    <div className="col-md-12 custom-pad-20">
-                                        <div className="tp-contact-form-field mb-20">
-                                            {/* <div className="bor"> */}
+                                        <div className="col-md-12 custom-pad-20">
+                                            <div className="tp-contact-form-field mb-20">
                                                 <label>Address</label>
                                                 <div class="row">
                                                     <div className="col-md-12 custom-pad-20">
                                                         <div className="tp-contact-form-field mb-20">
-                                                            <input type="text" placeholder="Street Address" />
+                                                            <input type="text" placeholder="Street Address" value={streetaddress} onChange={(e) => setStreetaddress(e.target.value)} />
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div class="row">
+                                                    <div className="col-md-12 custom-pad-20">
+                                                        <div className="tp-contact-form-field mb-20">
+                                                            <input type="text" placeholder="Street Address Line 2" value={streetaddress2} onChange={(e) => setStreetaddress2(e.target.value)} />
                                                         </div>
                                                     </div>
                                                 </div>
@@ -118,34 +189,34 @@ const ContactForm = () => {
                                                 <div className="row">
                                                     <div className="col-md-6 custom-pad-20">
                                                         <div className="tp-contact-form-field mb-20">
-                                                            <input type="text" placeholder="City" />
+                                                            <input type="text" placeholder="City" value={city} onChange={(e) => setCity(e.target.value)} />
                                                         </div>
                                                     </div>
                                                     <div className="col-md-6 custom-pad-20">
                                                         <div className="tp-contact-form-field mb-20">
-                                                            <input type="text" placeholder="State / Province" />
+                                                            <input type="text" placeholder="State / Province" value={state} onChange={(e) => setState(e.target.value)} />
                                                         </div>
                                                     </div>
                                                 </div>
                                                 <div class="row">
                                                     <div className="col-md-6 custom-pad-20">
                                                         <div className="tp-contact-form-field mb-20">
-                                                            <input type="text" placeholder="Postal / Zip Code" />
+                                                            <input type="text" placeholder="Postal / Zip Code" value={zip} onChange={(e) => setZip(e.target.value)} />
                                                         </div>
                                                     </div>
                                                 </div>
-                                            {/* </div> */}
+                                            </div>
                                         </div>
-                                    </div>
 
-                                    <div className="col-md-12 custom-pad-20">
-                                        <div className="tp-contact-form-field">
-                                            <button type="submit" className="theme-btn text-white">
-                                                <i className="flaticon-enter"></i> Send Message</button>
+                                        <div className="col-md-12 custom-pad-20">
+                                            <div className="tp-contact-form-field">
+                                                <button type="submit" className="theme-btn text-white">
+                                                    <i className="flaticon-enter"></i> Send Message</button>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
-                            </div>
+                            </form>
                         </div>
                     </div>
                 </div>
